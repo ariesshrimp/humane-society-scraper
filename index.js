195301b39c0744f34852237480fda0e1753c0d0b
joefraley <jose.fraley@gmail.com>
2016-11-27 14:44:37 -0800

    Proper promise chaining.

--- index.js
+++ index.js
@@ -2,38 +2,11 @@
 const fetch = require(`node-fetch`)
 const osmosis = require(`osmosis`)
 const { app, database } = require(`./database.js`)
+const { log, error, info } = console
 
-
-/**
- * You need a safety function because the Humane Society data is sometimes missing fields.
- * This is used extensively in the data() structure call below to properly format untamed
- * data structures before sending them to the database.
- * 
- * @param {any} dataField - the result of an attempted property access on a data object.
- * @param {function} parseFunction - a function for deciding how to improve the given data field
- * 
- * @return {any} - the result of the given parse function, or else null (in case no such field exists)
- */
 const parse = (dataField, parseFunction) => dataField ? parseFunction(dataField) : null
 
-/** 
- * A simple store for comparing the keys of yesterday's entries with todays.
- * This is used to decide whether an animal should be removed from the database:
- * if it has 'disappeard' from the website.
- */
-const newEntries = []
-
-/**
- * Reformat all the results into well-structured data for the database.
- * Some of the values may be missing if the Humane Society hasn't been able
- * to determine the information yet, or if the species makes knowing
- * the answer too difficult / impossible (e.g. bird weight)
- * 
- * @param {object} data - an in memory representation of an animal from a scrape result
- * 
- * @return {object} newEntry - a reference to a Firebase object containing updated fields. 
- */
-const updateDB = data => {
+const normalize = data => new Promise(resolve => {
   const color = parse(data.color, color => color.split(`, `).map(color => color ? color.toUpperCase() : ``))
   const species = parse(data.species, species => {
     const uppercased = species.toUpperCase()
@@ -42,20 +15,15 @@ const updateDB = data => {
     else return uppercased.replace(` `, `_`)
   })
 
-    /**
-     * adopt_fee, age, and weight can all fail parseFloat, so they require a fallback return value.
-     * sex can effectively fail if the humane soceity is unable to determine the sex easily (such as with birds) 
-     * I've decided to use null
-     */
   const adopt_fee = parse(data.adopt_fee, fee => parseFloat(fee.substring(1) || null))
   const age = parse(data.age, age => age
-        .split(` `)
-        .map((number, index, array) => {
-          if (array[index + 1] === `years`) return parseInt(number) * 12
-          if (array[index + 1] === `months`) return parseInt(number)
-          else return 0
-        })
-        .reduce((x, y) => x + y, 0) || null)
+    .split(` `)
+    .map((number, index, array) => {
+      if (array[index + 1] === `years`) return parseInt(number) * 12
+      if (array[index + 1] === `months`) return parseInt(number)
+      else return 0
+    })
+    .reduce((x, y) => x + y, 0) || null)
   const weight = parse(data.weight, weight => parseFloat(weight) || null)
 
   const sex = parse(data.sex, sex => {
@@ -64,82 +32,109 @@ const updateDB = data => {
     else return uppercased
   })
 
-    /**
-     * Get a new structure with the formated values updated
-     */
-  const improvedData = Object.assign({}, data, {
+  const normalizedData = Object.assign({}, data, {
     adopt_fee,    
     age,    
-    color,
-    friends: [], // TODO:jmf implement this. It doesn't look like there's a well-structured dataset for friend connections.    
+    color,    
     sex,
     species,
     weight
   })
 
-    /**
-     * Index the new id to cross check for clearing old entries
-     */
-  newEntries.push(improvedData.id)
-  const newEntry = database.ref(`animals/${ improvedData.id }`).update(improvedData)
-  return newEntry
-}
+  resolve(normalizedData)
+})
 
-/**
- * Cross reference the newly found animal ID's witht those that existed when we started 
- * and remove all the animals that have been removed since the last update
- * 
- * @param {array} newKeys - a list of unique animal ID's we've acquired during the most recent scrap
- * 
- * @return {promise} - a promise to kill any animals in the old set not in the newest data set.
- */
-const clearOldEntries = newKeys => {
-  console.log(`
+const sendToDB = data => database.ref(`animals/${ data.id }`).set(data)
+
+const clearOldEntries = (newKeys, data) => {
+   log(`
   
   starting to clear old entries
   
   `)
   return fetch(`https://humane-society-scrape.firebaseio.com/animals.json?shallow=true`)
-    .then(response => response.json())
-    .then(Object.keys)
+    .then(response => response.json())    
+    .then(result => {
+      try {
+        const keys = Object.keys(result)
+        return keys
+      }
+      catch(error) {
+        return []
+      }
+    })
     .then(oldKeys => oldKeys.filter(key => !newKeys.includes(key)))
     .then(keysToRemove => {
       return Promise.all(keysToRemove.map(key => {
         return database.ref(`animals/${key}`).remove()
       }))
     })
+    .then(() => data)
 }
 
 
-/**
- * The human society divides its site up into a few pages storing different general groups of animals.
- * @see http://www.oregonhumane.org/adopt/?type=small or
- * @see @see http://www.oregonhumane.org/adopt/?type=dog
- * 
- * So a function is needed to repeat the scrape process on any given page (or for a given animal type).
- * 
- * @param {string} type - a string representing the kind of animal being scraped for.
- * 
- * @return {object} osmosis - the scraper instance. not very useful for returning.
- */
 const scrape = type => {
   const url = `http://www.oregonhumane.org/adopt/?type=${type}`
+  let results = []
 
   return new Promise((resolve, reject) => osmosis
   .get(url)
   .find(`div.result-item > a`)
   .follow(`@href`) // follow links to each indiviudal animal's listing page
-
   .find(`.animal-details`)
   .set({
     description: `.detail-desc p`,          
     image_url: `.detail-image img @src`
   })
-
   .find(`.detail-text`).set({ name: `h2` })
-
   .find(`.detail-stats table tbody`)
-  .set({
+  .set({ /** @see below for explanation of this crazy xPath syntax */
+    adopt_fee: `//*[text()[contains(., 'Adopt Fee')]]/parent::tr/*[last()]`,
+    age: `//*[text()[contains(., 'Age')]]/parent::tr/*[last()]`,
+    breed: `//*[text()[contains(., 'Breed')]]/parent::tr/*[last()]`,
+    color: `//*[text()[contains(., 'Color')]]/parent::tr/*[last()]`,
+    date_available: `//*[text()[contains(., 'Date Available')]]/parent::tr/*[last()]`,
+    id: `tr[last()] td`,
+    sex: `//*[text()[contains(., 'Sex')]]/parent::tr/*[last()]`,
+    species: `//*[text()[contains(., 'Type')]]/parent::tr/*[last()]`,
+    weight: `//*[text()[contains(., 'Weight')]]/parent::tr/*[last()]`
+  })
+  .then((context, data, next) => {
+    results.push(normalize(data))
+    next(context, data)
+  })
+  .done(() => resolve(Promise.all(results)))
+  .error(reject)
+)}
+
+
+// const types = [`small`, `horsefarm`, `dogs`, `cats`]
+const types = [`small`, `horsefarm`]
+const promises = types.map(scrape)
+const update = () => Promise.all(promises)
+  .then(results => results.reduce((accumulator, part) => {
+    return [...accumulator, ...part]
+  }, []))  
+  .then(results => clearOldEntries(results.map(result => result.id), results))
+  .then(results => Promise.all(results.map(sendToDB)))
+  .then(() => app.delete())
+  .catch(error)
+
+update()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * https://msdn.microsoft.com/en-us/library/ms256060(v=vs.110).aspx
  * The xPath pattern below translates to something like this in English:
@@ -163,41 +158,3 @@ const scrape = type => {
  *  </table>
  * </div>
  */
-    adopt_fee: `//*[text()[contains(., 'Adopt Fee')]]/parent::tr/*[last()]`,
-    age: `//*[text()[contains(., 'Age')]]/parent::tr/*[last()]`,
-    breed: `//*[text()[contains(., 'Breed')]]/parent::tr/*[last()]`,
-    color: `//*[text()[contains(., 'Color')]]/parent::tr/*[last()]`,
-    date_available: `//*[text()[contains(., 'Date Available')]]/parent::tr/*[last()]`,
-    id: `tr[last()] td`,
-    sex: `//*[text()[contains(., 'Sex')]]/parent::tr/*[last()]`,
-    species: `//*[text()[contains(., 'Type')]]/parent::tr/*[last()]`,
-    weight: `//*[text()[contains(., 'Weight')]]/parent::tr/*[last()]`
-  })
-  .data(updateDB) // Send each individual animal to the DB 
-  .log(console.log)
-  .debug(console.info)  
-  .done(resolve)  
-  .error(reject)
-)}
-
-
-const types = [`small`, `horsefarm`, `dogs`, `cats`]
-const promises = types.map(scrape)
-const update = () => Promise.all(promises)
-  .then(() => {
-    console.log(`
-
-  All the promises are done!
-
-`)
-    app.delete()
-  })
-// ^ app.delete() tells firebase to close open sockets and clear the resources,
-// allowing the node process to properly exit when finished.
-  .catch(error => console.error(`
-
-  Something weird happened...
-  ${ error }
-`))
-
-update()
